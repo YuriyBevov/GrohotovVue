e622e4fa94eb56ba0641a979ab0bba815e501d08
Yuriy Bevov <jury19y@mail.ru>
2021-09-26 16:38:56 +0300

    удалил 404

--- src/main.js
+++ src/main.js
@@ -8,12 +8,5 @@ Vue.config.productionTip = false
 new Vue({
   store,
   router,
-  render: h => h(App),
-  created () {
-      if (sessionStorage.redirect) {
-          const redirect = sessionStorage.redirect
-          delete sessionStorage.redirect
-          this.$router.push(redirect)
-      }
-  }
+  render: h => h(App)
 }).$mount('#app')

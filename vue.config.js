ea3b63cd349e85755fb2aa194b9e0a3eaf8c281e
Yuriy Bevov <jury19y@mail.ru>
2021-09-27 21:51:49 +0300

    upd vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 // vue.config.js
 
-/**
- * @type {import('@vue/cli-service').ProjectOptions}
- */
-
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? '/GrohotovVue/dist/'
